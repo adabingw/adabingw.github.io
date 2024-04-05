fd1d65afde00f8bedcb1bee002319273b96564f4
Ada <adabingw@yahoo.com>
2024-04-05 08:11:22 -0400

    :sake:

--- adabingw/src/App.js
+++ adabingw/src/App.js
@@ -4,13 +4,7 @@ import './rain.css';
 import { useState, useEffect } from 'react';
 import $ from 'jquery';
 
-import Project from './Project';
 import Work from './Work';
-
-import Email from './img/email.jpg';
-import Github from './img/github.jpg';
-import Linkedin from './img/linkedin.jpg';
-import Resume from './img/resume.jpg';
 
 import Moon from './img/moon.jpg'; 
 import Rain from './img/rain.png'; 
@@ -41,9 +35,6 @@ function App() {
       drops += '<div class="drop" style="color: #1F51FF; left: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
       backDrops += '<div class="drop" style="right: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
     }
-
-    console.log("INCREMENTS")
-    console.log(drops)
 
     $('.rain.front-row').append(drops);
     $('.rain.back-row').append(backDrops);
